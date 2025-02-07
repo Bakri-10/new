78ca622c31cccbb86d33ae97c27a72d9aaf3524d
Marcel <66844942+Bakri-10@users.noreply.github.com>
2025-02-07 15:39:03 -0600

    Update test.tf

--- test.tf
+++ test.tf
@@ -30,11 +30,31 @@ variable "location" {
   default = "eastus2"
 }
 
-variable "purpose" {
+variable "secondary_location"{
   type = string
-  description = "purpose/sequence (format: purpose/sequence, e.g. test/01)"
-  default = "test/01"
+  description = "location"
+  default = "eastus2"
 }
+
+variable "purpose" {
+  type        = string
+  default     = "default"
+  description = "(Required) The VM Role and Sequence for naming use or, submit the desired resource name (one that contains a dash)."
+  validation {
+    condition     = strcontains(var.purpose, "-") ? length(var.purpose) <= 30 : length(var.purpose) <= 10
+    error_message = "(Required) Purpose [VM Role.Sequence] segment cannot exceed 10 characters. Name cannot exceed 80."
+  }
+ }
+
+variable "purpose_rg" {
+  type        = string
+  default     = "default"
+  description = "(Required) The purpose segment of the Resource Group name. Should not exceed 5 characters."
+  validation {
+    condition     = strcontains(var.purpose_rg, "-") ? length(var.purpose_rg) <= 80 : length(var.purpose_rg) <= 5
+    error_message = "(Required) Purpose segment cannot exceed 5 characters. Name cannot exceed 80."
+  }  
+ }
 
 variable "db_purpose" {
   type = list(string)
